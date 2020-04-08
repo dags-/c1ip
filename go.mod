86225612b95178e87ed92cb259e020b3740a3c61
dags- <dags@dags.me>
2020-04-08 14:17:03 +0100

    ffmpeg transcode

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module c1ip
 
-go 1.13
-
-require github.com/pkg4go/httprange v0.0.0-20160404143851-bf50c5def1ce // indirect
+go 1.13ó
--- main.go
+++ main.go
@@ -9,29 +9,26 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"net/http/httputil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"time"
 )
 
-var (
-	port  = flag.Int("port", 0, "Server port")
-	debug = flag.Bool("debug", false, "debugging")
-)
-
 func main() {
-	flag.Parse()
+	_ = os.Mkdir("temp", os.ModePerm)
 	_ = os.Mkdir("video", os.ModePerm)
+	port := flag.Int("port", 0, "Server port")
+	flag.Parse()
 	l, e := net.Listen("tcp", fmt.Sprintf("localhost:%v", *port))
 	if e != nil {
 		panic(e)
 	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", serve)
-	mux.Handle("/manage/", http.StripPrefix("/manage/", http.FileServer(http.Dir("html"))))
-	mux.HandleFunc("/manage/upload", upload)
-	log.Println("Serving at:", "http://"+l.Addr().String()+"/manage/")
+	mux.Handle("/", http.FileServer(http.Dir("video")))
+	mux.Handle("/upload/", http.StripPrefix("/upload/", http.FileServer(http.Dir("html"))))
+	mux.HandleFunc("/upload/file", upload)
+	log.Println("http://" + l.Addr().String() + "/upload")
 	e = http.Serve(l, mux)
 	if e != nil {
 		panic(e)
@@ -44,31 +41,47 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, e.Error(), http.StatusNotFound)
 		return
 	}
+
 	in, _, e := r.FormFile("upload")
 	if e != nil {
 		http.Error(w, e.Error(), http.StatusNotFound)
 		return
 	}
 	defer doClose(in)
-	token := nextId()
-	path := filepath.Join("video", token)
-	out, e := os.Create(path)
+
+	name := nextId()
+	src := filepath.Join("temp", name)
+	dest := filepath.Join("video", name+".mp4")
+	out, e := os.Create(src)
 	if e != nil {
 		http.Error(w, e.Error(), http.StatusNotFound)
 		return
 	}
-	defer doClose(out)
 	_, _ = io.Copy(out, in)
-	http.Redirect(w, r, "/"+token, 301)
+	doClose(out)
+
+	logErr(convert(src, dest))
+	logErr(os.Remove(src))
+
+	http.Redirect(w, r, "/"+name+".mp4", 302)
 }
 
-func serve(w http.ResponseWriter, r *http.Request) {
-	if *debug {
-		dump(r)
-	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	path := filepath.Join("video", r.URL.Path)
-	http.ServeFile(w, r, path)
+func convert(src, dest string) error {
+	c := exec.Command(
+		"ffmpeg",
+		"-i", src,
+		"-c:v", "libx264",
+		"-b:v", "4M", "-maxrate", "4M", "-bufsize", "1M",
+		"-vf", "scale=-1:720:flags=lanczos",
+		"-preset", "fast",
+		dest,
+	)
+	er, _ := c.StderrPipe()
+	go func() {
+		defer doClose(er)
+		_, _ = io.Copy(os.Stdout, er)
+	}()
+	return c.Run()
 }
 
 func doClose(c io.Closer) {
@@ -84,14 +97,7 @@ func logErr(e error) {
 }
 
 func nextId() string {
-	data := make([]byte, 8)
-	binary.LittleEndian.PutUint64(data, uint64(time.Now().UnixNano()))
+	data := make([]byte, 4)
+	binary.LittleEndian.PutUint32(data, uint32(time.Now().Unix()))
 	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(data)
-}
-
-func dump(r *http.Request) {
-	data, e := httputil.DumpRequest(r, true)
-	if e == nil {
-		log.Println(string(data))
-	}
 }
