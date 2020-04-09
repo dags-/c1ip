53bc8f7ff779ae5fde2fecc2cfd7904af27f5679
dags- <dags@dags.me>
2020-04-09 23:38:38 +0100

    templates

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 	"time"
 )
@@ -20,7 +22,15 @@ import (
 var (
 	port = flag.Int("port", 8088, "Server port")
 	addr = flag.String("url", "", "Website url")
-	page = template.Must(template.ParseFiles("html/template.html"))
+	page = template.Must(template.ParseFiles("html/video/template.html"))
+	home = template.Must(template.New("template.html").Funcs(template.FuncMap{
+		"name": func(s string) string {
+			return strings.TrimSuffix(s, ".mp4")
+		},
+		"date": func(t time.Time) string {
+			return t.Format(time.Stamp)
+		},
+	}).ParseFiles("html/home/template.html"))
 )
 
 func main() {
@@ -33,9 +43,8 @@ func main() {
 	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", serve)
-	mux.Handle("/upload/", http.StripPrefix("/upload/", http.FileServer(http.Dir("html"))))
-	mux.HandleFunc("/upload/file", upload)
-	log.Println("http://" + l.Addr().String() + "/upload")
+	mux.HandleFunc("/upload", upload)
+	log.Println("http://" + l.Addr().String())
 	e = http.Serve(l, mux)
 	if e != nil {
 		panic(e)
@@ -43,11 +52,20 @@ func main() {
 }
 
 func serve(w http.ResponseWriter, r *http.Request) {
-	if strings.LastIndexAny(r.URL.Path, "./") > 0 {
-		if r.URL.Path == "/favicon.ico" {
-			http.ServeFile(w, r, filepath.Join("html", r.URL.Path))
-		} else {
+	if r.URL.Path == "/" || r.URL.Path == "/index.html" {
+		files, e := listFiles()
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+		}
+		e = home.Execute(w, files)
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+		}
+	} else if strings.LastIndexAny(r.URL.Path, "./") > 0 {
+		if strings.HasSuffix(r.URL.Path, ".mp4") {
 			http.ServeFile(w, r, filepath.Join("video", r.URL.Path))
+		} else {
+			http.ServeFile(w, r, filepath.Join("html", r.URL.Path))
 		}
 	} else {
 		e := page.Execute(w, *addr+r.URL.Path)
@@ -58,33 +76,41 @@ func serve(w http.ResponseWriter, r *http.Request) {
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
-	e := r.ParseMultipartForm(200 << 20)
-	if e != nil {
-		http.Error(w, e.Error(), http.StatusNotFound)
+	if r.Method == http.MethodGet {
+		http.ServeFile(w, r, filepath.Join("html", r.URL.Path))
 		return
 	}
 
-	in, _, e := r.FormFile("upload")
-	if e != nil {
-		http.Error(w, e.Error(), http.StatusNotFound)
+	if r.Method == http.MethodPost {
+		e := r.ParseMultipartForm(200 << 20)
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusNotFound)
+			return
+		}
+
+		in, _, e := r.FormFile("upload")
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusNotFound)
+			return
+		}
+		defer doClose(in)
+
+		name := nextId()
+		src := filepath.Join("temp", name)
+		dest := filepath.Join("video", name+".mp4")
+		out, e := os.Create(src)
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusNotFound)
+			return
+		}
+		_, _ = io.Copy(out, in)
+		doClose(out)
+
+		go convert(src, dest)
+
+		http.Redirect(w, r, "/"+name, 302)
 		return
 	}
-	defer doClose(in)
-
-	name := nextId()
-	src := filepath.Join("temp", name)
-	dest := filepath.Join("video", name+".mp4")
-	out, e := os.Create(src)
-	if e != nil {
-		http.Error(w, e.Error(), http.StatusNotFound)
-		return
-	}
-	_, _ = io.Copy(out, in)
-	doClose(out)
-
-	go convert(src, dest)
-
-	http.Redirect(w, r, "/"+name, 302)
 }
 
 func convert(src, dest string) {
@@ -99,6 +125,17 @@ func convert(src, dest string) {
 	)
 	logErr(c.Run())
 	logErr(os.Remove(src))
+}
+
+func listFiles() ([]os.FileInfo, error) {
+	files, e := ioutil.ReadDir("video")
+	if e != nil {
+		return nil, e
+	}
+	sort.Slice(files, func(i, j int) bool {
+		return files[j].ModTime().Unix() < files[i].ModTime().Unix()
+	})
+	return files, nil
 }
 
 func doClose(c io.Closer) {
